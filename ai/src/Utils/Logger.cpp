cbd2f066182e5c69a8a650d111ed4ddf8ebf6b40
Mathis Bukowski <mathis.bukowski@epitech.eu>
2025-07-03 11:26:13 +0200

    feat(logger): implement Logger class with debug message functionality

--- ai/src/Utils/Logger.cpp
+++ ai/src/Utils/Logger.cpp
@@ -6,3 +6,5 @@
 */
 
 #include "Logger.hpp"
+
+rustiq::Logger::Logger(const bool& isDebugActivate): _isDebuggingMode(isDebugActivate) {}
--- ai/src/Utils/Logger.hpp
+++ ai/src/Utils/Logger.hpp
@@ -7,12 +7,27 @@
 
 #ifndef LOGGER_HPP
 #define LOGGER_HPP
+#include <cstddef>
+#include <string>
+#include <iostream>
 
+namespace rustiq {
+    class Logger {
+    public:
+        explicit Logger(const bool &isDebugActivate);
+        ~Logger() = delete;
 
-
-class Logger {
-
-};
+        template <typename T>
+        void sendDebugMessage(const T& dataToSend, std::size_t lenght, const std::string& label)
+        {
+            if (!this->_isDebuggingMode)
+                return;
+            std::cout << "[DEBUG] " << label << "(" << lenght << "bytes): " << dataToSend << std::endl;
+        }
+    private:
+        bool _isDebuggingMode;
+    };
+}
 
 
 
