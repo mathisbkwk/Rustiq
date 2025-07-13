541727cff744bea540053bba584887a25c695227
Mathis Bukowski <mathis.bukowski@epitech.eu>
2025-07-13 15:16:09 +0200

    feat: adding tensor class

--- ai/src/nn/Tensor.hpp
+++ ai/src/nn/Tensor.hpp
@@ -10,9 +10,43 @@
 
 
 
-class Tensor {
+#include <functional>
+#include <memory>
+#include <vector>
 
-};
+
+namespace rustiq {
+    class Tensor {
+        public:
+            Tensor(const std::vector<int>& shape);
+            Tensor(const std::vector<float>& data, const std::vector<int>& shape);
+            Tensor() = default;
+            
+            ~Tensor();
+    
+            Tensor operator+(const Tensor& other) const;
+            Tensor operator-(const Tensor& other) const;
+            Tensor operator*(const Tensor& other) const;
+            
+            float& operator[](const std::vector<int>& indices);
+            const float& operator[](const std::vector<int>& indices) const;
+            
+            void reshape(const std::vector<int>& new_shape);
+            Tensor transpose();
+            std::vector<int> get_shape() const;
+            size_t size() const;
+            void backward(const Tensor& grad);
+            
+            std::shared_ptr<Tensor> grad;
+            bool _requires_grad;
+        private:
+            std::vector<float> _data;
+            std::vector<int> _shape;
+            std::vector<int> _strides;
+            std::function<void(const Tensor &)> _backward;
+            std::vector<std::shared_ptr<Tensor>> _parents;
+    };
+}
 
 
 
